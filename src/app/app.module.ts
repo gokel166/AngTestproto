5c94ef699659513cd8c30a9b5abaf33b4bd6afce
George Okello <okellogeorge40@yahoo.com>
2018-01-29 04:46:17 -0600

    Adding clubs component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ClubsComponent } from './clubs/clubs.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClubsComponent
   ],
   imports: [
     BrowserModule
